1400a089ed8eb7c138f05973a2498516abfed8b4
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-17 19:33:27 +0200

    Mise en place de la methode direUno

--- app/src/main/java/uno/Joueur.java
+++ app/src/main/java/uno/Joueur.java
@@ -2,10 +2,13 @@ package uno;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Joueur {
     private String nomJoueur;
     private List<Carte> main;
+
+    Scanner scanner = new Scanner(System.in);
 
     public Joueur(String nomJoueur) {
         this.nomJoueur = nomJoueur;
@@ -28,9 +31,23 @@ public class Joueur {
         main.remove(carte);
     }
 
+    public List<Carte> getMain() {
+        return main;
+    }
+
 
     public void direUno() {
-
+        if(getNombreCartes() == 1 ){
+            System.out.println("Voullez vous dire Uno ?");
+            int choix = scanner.nextInt();
+            if(choix == 1){
+                System.out.println("Uno !");
+            } else {
+                System.out.println("Vous avez oublié de dire Uno !");
+                piocherCarte();
+                piocherCarte();
+            }
+        }
     }
 
     public boolean gagnant() {
