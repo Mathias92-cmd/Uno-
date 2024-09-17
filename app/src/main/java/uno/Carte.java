981077a8cf22c57fcd4e16ec3be2ae14e4ea29ad
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-17 18:59:30 +0200

    Classe Carte et Joueur

---
+++ app/src/main/java/uno/Carte.java
@@ -0,0 +1,14 @@
+package uno;
+
+public abstract class Carte {
+    private String couleur;
+    private String valeur;
+
+    public void appliqueEffet() {
+
+    }
+
+    public boolean estJouable(Carte carte) {
+
+    }
+}
---
+++ app/src/main/java/uno/Joueur.java
@@ -0,0 +1,24 @@
+package uno;
+
+import java.util.List;
+
+public class Joueur {
+    private String nomJoueur;
+    private List<Carte> main;
+
+    public void jouerCarte(Carte carte) {
+
+    }
+
+    public Carte piocherCarte() {
+
+    }
+
+    public void direUno() {
+
+    }
+
+    public boolean gagnant() {
+        return false;
+    }
+}
