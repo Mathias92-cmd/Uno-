29978ef9f9534151c7ac2d89e7a8d36475a0f960
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-18 18:49:16 +0200

    Methode appliquerEffetPlusDeux / nouvelle variable

--- app/src/main/java/uno/Carte.java
+++ app/src/main/java/uno/Carte.java
@@ -3,14 +3,16 @@ package uno;
 public abstract class Carte {
     private String couleur;
     private String valeur;
+    private int nbCartes;
 
     public Carte(String couleur, String valeur) {
         this.couleur = couleur;
         this.valeur = valeur;
+        nbCartes = 0;
     }
 
-    public void appliqueEffet() {
-
+    public int getNbCartes() {
+        return nbCartes;
     }
 
     public boolean estJouable(Carte carte) {
--- app/src/main/java/uno/CarteSpeciale.java
+++ app/src/main/java/uno/CarteSpeciale.java
@@ -2,16 +2,38 @@ package uno;
 
 public class CarteSpeciale extends Carte {
 
-    private String effet;
+    public enum Effet {
+        PlusDeux,
+        PlusQuatre,
+        InversionSens,
+        ChangementCouleur
+    }
 
-    public CarteSpeciale(String couleur, String effet) {
+    private Effet effet;
+
+    public CarteSpeciale(String couleur , Effet effet) {
         super(couleur, "");
         this.effet = effet;
     }
 
-    public void appliquerEffet() {
-
+    public void appliquerEffetPlusDeux(Carte carte) {
+        int nbCarte = getNbCartes();
+        if (carte instanceof CarteSpeciale) {
+            Effet effet = ((CarteSpeciale) carte).getEffet();
+            if (effet == Effet.PlusDeux) {
+                System.out.println("Le joueur suivant pioche 2 cartes");
+                nbCarte += 2;
+            } else if (effet == Effet.PlusQuatre) {
+                System.out.println("Le joueur suivant pioche 4 cartes");
+                nbCarte += 4;
+            }
+        }
     }
+
+    private Effet getEffet() {
+        return effet;
+    }
+
 
     public boolean estJouable(Carte carte) {
         return false;
