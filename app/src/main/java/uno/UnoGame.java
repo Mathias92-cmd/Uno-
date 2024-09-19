14a22aa76fa17c2ad8cba05810b57bd200060ab4
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-19 14:37:37 +0200

    Changement 2.0 sur la methode UnoGame

--- app/src/main/java/uno/UnoGame.java
+++ app/src/main/java/uno/UnoGame.java
@@ -1,20 +1,34 @@
 package uno;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UnoGame {
     private Joueur joueurCourant;
     private Deck deck;
     private Pioche piocheJetable;
+    ArrayList<Joueur> joueurs = new ArrayList<>();
+    private int indexJoueurCourant;
 
+    public int getIndexJoueurCourant() {
+        return indexJoueurCourant;
+    }
 
     public UnoGame() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Entrez le nom du joueur: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez le nombre de joueurs: ");
+        int nombreJoueurs = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < nombreJoueurs; i++) {
+            System.out.print("Entrez le nom du joueur " + (i + 1) + ": ");
             String nomJoueur = scanner.nextLine();
-            this.joueurCourant = new Joueur(nomJoueur);
-            this.deck = new Deck();
-            this.piocheJetable = new Pioche();
+            joueurs.add(new Joueur(nomJoueur));
+        }
+        this.indexJoueurCourant = 0;
+        this.joueurCourant = joueurs.get(indexJoueurCourant);
+        this.deck = new Deck();
+        this.piocheJetable = new Pioche();
     }
 
 
@@ -29,7 +43,6 @@ public class UnoGame {
     public void finDeLaPartie() {
         if(joueurCourant.getNombreCartes() == 0){
             System.out.println("Le joueur " + joueurCourant.getNomJoueur() + " a gagné !");
-            System.out.println("Fin de la partie");
         }
     }
 }
