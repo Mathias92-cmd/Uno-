0aabaacae9aab3089e90de72d2f34e1ff005d169
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-20 21:14:41 +0200

    Modif methode ajouterCarte et getCarteEnJeu

--- app/src/main/java/uno/Pioche.java
+++ app/src/main/java/uno/Pioche.java
@@ -6,15 +6,18 @@ public class Pioche {
 
     private List<Carte> cartes;
 
+    private Carte carteEnJeu;
+
     private UnoGame unoGame;
 
     public void ajouterCarte(Carte carte) {
         int nbCarteADistribuer = 7 * unoGame.getNbJoueurs();
         cartes.add(carte);
+        carteEnJeu = carte;
         System.out.println("Nombre de cartes restantes: " + (cartes.size() - nbCarteADistribuer));
     }
 
-    public Carte getDerniereCarte() {
-
+    public Carte getCarteEnJeu() {
+        return carteEnJeu;
     }
 }
