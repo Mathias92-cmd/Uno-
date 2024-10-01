d464234df29c562b3345f275f133c16fef7b64c9
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-10-01 18:11:12 +0200

    Ajout javaDoc

--- app/src/main/java/uno/main.java
+++ app/src/main/java/uno/main.java
@@ -3,6 +3,9 @@ package uno;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+/**
+ * Classe principale pour démarrer le jeu Uno.
+ */
 public class main {
     public static void main(String[] args) {
         UnoGame jeu = new UnoGame();
